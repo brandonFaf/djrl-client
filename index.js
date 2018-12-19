736072d35a2f1e5a9f4884b80bc532d0636e7a55
Brandon <brandonstrash91@yahoo.com>
2018-12-18 21:28:15 -0500

    add express for hosting on heroku

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+let app = express();
+var port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log("listening on port 5000");
+});
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
+}
